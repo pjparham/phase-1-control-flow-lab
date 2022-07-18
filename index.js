ae5bf06db1f86c221d1f4b49844b1bc14453f387
Parker Parham <parkerparham@gmail.com>
2022-07-18 15:25:33 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+  if (feet <= 400){
+    return `This one is on me!`
+  }
+  else if (feet > 400 && feet < 2000){
+    return 'That will be twenty bucks.'
+  }
+  else if (feet > 2000 && feet < 2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (feet > 2500){
+    return 'No can do.'
+  }
+  // Write your code here!
+}
+function ternaryCheckCity(city){
+  return city === "NYC"?'Ok, sounds good.':`No go.`
+}
+
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'    
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+// function ternaryCheckCity(city){
+//   return city === NYC?'Ok, sounds good.':`No go.`
+// } this is what I had that wasn't working. Remember to wrap things as strings!
